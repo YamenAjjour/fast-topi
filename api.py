e98f9679edb1ab1ff06e65f35c9d946ec7376b58
befi8957 <yamen.ajjour@uni-weimar.de>
2021-12-12 13:07:46 +0100

    setting api parameter

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 app = FastAPI()
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+
+@app.get("/topics/")
+async def read_item(title: str = Query("no-title",min_length=3,max_length=50)):
+    return {"topic":title}
 
