1e54c4a8eaba2efe8716142ac0e1fc10909d2900
befi8957 <yamen.ajjour@uni-weimar.de>
2021-12-12 13:34:28 +0100

    preprocessingd dataset

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ app = FastAPI()
 
 
 @app.get("/topics/")
-async def read_item(title: str = Query("no-title",min_length=3,max_length=50)):
+def read_item(title: str = Query("no-title",min_length=3,max_length=50)):
     return {"topic":title}
+
 
